858805882fb3f1b7868cc8dbfca8e0203cee5991
Chandan S <148647969+ChandanShanthkumar@users.noreply.github.com>
2024-12-11 14:28:38 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,3 +1,6 @@
+#Internship completion project P2 Attendance management system
+#for successful execution please make a file named photos with all photos uploaded
+# then test the project in the same project directory and CSV file path.
 import cv2
 import face_recognition
 import os
